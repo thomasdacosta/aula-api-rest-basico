76653946f612f83427fe0cf6e10d6ab73ca7efa1
Thomas da Costa <thomasdacosta@gmail.com>
2022-03-27 18:14:01 -0300

    first commit

---
+++ src/main/java/edu/senac/SenacApiRestBasicoApplication.java
@@ -0,0 +1,13 @@
+package edu.senac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenacApiRestBasicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenacApiRestBasicoApplication.class, args);
+	}
+
+}
